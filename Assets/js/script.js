d75709311904333a4d5eaf828b41e45302781409
Dinhn17 <nquydinh@yahoo.com>
2024-01-15 15:57:24 +1030

    added currentDay as advancedFormat

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+// import advancedFormat from "dayjs/plugin/advancedFormat";
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -20,4 +21,11 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  // var now = dayjs();
+  // var customParseFormat = require('dayjs/plugin/customParseFormat')
+  // var advancedFormat = require('dayjs/plugin/advancedFormat');
+  // dayjs.extend(advancedFormat);
+  var reformatDate = dayjs().format('dddd, MMMM Do');
+  $('#currentDay').text(reformatDate);
+  console.log(now);
 });
